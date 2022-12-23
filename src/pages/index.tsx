563114ff765cc07a114c75ff52d3a92a12aa43f3
ianbrdeguzman <ianbrdeguzman@gmail.com>
2022-12-23 13:59:42 -0500

    update styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,9 @@ import { useAuthUser } from '@react-query-firebase/auth';
 export default function Home() {
   const { data: user } = useAuthUser(['user'], auth);
 
-  console.log(user?.email);
-
   return (
-    <div>
+    <div className="pt-[4rem]">
       <h1 className="text-3xl font-bold underline">Hello {user?.email}</h1>
-      <button>Sign out</button>
     </div>
   );
 }
