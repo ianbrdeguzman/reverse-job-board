d3201e6463dcefc967bf41628cf679965ec86a87
ianbrdeguzman <ianbrdeguzman@gmail.com>
2022-12-24 00:04:45 -0500

    setup firebase admin sdk

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -1,23 +1,19 @@
 import { initializeApp, cert, getApps, getApp } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getAuth } from 'firebase-admin/auth';
-import { config } from './config';
+import { config } from '../config';
 
-if (getApps().length === 0) {
-  initializeApp(
-    process.env.NODE_ENV !== 'production'
-      ? { projectId: 'reverse-job-board' }
-      : {
-          credential: cert({
-            privateKey: config.privateKey,
-            clientEmail: config.clientEmail,
-            projectId: config.projectId
-          })
-        }
-  );
-} else {
-  getApp();
-}
+getApps().length === 0
+  ? process.env.NODE_ENV !== 'production'
+    ? initializeApp({ projectId: config.firebase.projectId })
+    : initializeApp({
+        credential: cert({
+          privateKey: config.firebase.privateKey,
+          clientEmail: config.firebase.clientEmail,
+          projectId: config.firebase.projectId
+        })
+      })
+  : getApp();
 
-export const firestoreAdmin = getFirestore();
-export const authAdmin = getAuth();
+export const firestore = getFirestore();
+export const auth = getAuth();
