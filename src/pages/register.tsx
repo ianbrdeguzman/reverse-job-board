8e1f9286a82e5822ae3505784450424a07573744
ianbrdeguzman <ianbrdeguzman@gmail.com>
2022-12-26 21:32:21 -0500

    clean up sign in and register pages

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Link from 'next/link';
+import { config } from '../config';
 import { getCookie } from 'cookies-next';
 import { auth } from '../firebase/admin';
 import { useAuth } from '../hooks/useAuth';
 import { GetServerSidePropsContext } from 'next';
-import { config } from '../config';
 
 export default function RegisterPage() {
   const { register, isLoading, isError, isSuccess } = useAuth();
@@ -18,72 +18,61 @@ export default function RegisterPage() {
     <div className="h-screen flex justify-center items-center">
       <div className="w-full max-w-md">
         <div>
-          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-800">
             Register an account
           </h2>
-          <p className="mt-2 text-center text-sm text-gray-600">
-            {`Or `}
-            <Link
-              href={config.routes.signin}
-              className="font-medium text-orange-500 hover:text-orange-400"
-            >
-              sign in to your account
-            </Link>
-          </p>
+          <Link
+            className="block mt-2 text-center text-sm font-medium text-orange-400 hover:text-orange-500"
+            href={config.routes.signin}
+          >
+            Or sign in to your account
+          </Link>
         </div>
         <form className="mt-8 space-y-6" onSubmit={handleOnSubmit}>
           <div className=" rounded-md shadow-sm">
-            <div>
-              <label htmlFor="email-address" className="sr-only">
-                Email address
-              </label>
-              <input
-                id="email-address"
-                name="email"
-                type="email"
-                autoComplete="email"
-                required
-                className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-orange-500 focus:outline-none focus:ring-orange-500 sm:text-sm"
-                placeholder="Email address"
-              />
-            </div>
-            <div>
-              <label htmlFor="password" className="sr-only">
-                Password
-              </label>
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                required
-                className="relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-orange-500 focus:outline-none focus:ring-orange-500 sm:text-sm"
-                placeholder="Password"
-              />
-            </div>
-            <div>
-              <label htmlFor="confirm-password" className="sr-only">
-                Confirm password
-              </label>
-              <input
-                id="confirm-password"
-                name="confirm-password"
-                type="password"
-                autoComplete="current-password"
-                required
-                className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-orange-500 focus:outline-none focus:ring-orange-500 sm:text-sm"
-                placeholder="Confirm password"
-              />
-            </div>
+            <label htmlFor="email-address" className="sr-only">
+              Email address
+            </label>
+            <input
+              id="email-address"
+              name="email"
+              type="email"
+              autoComplete="email"
+              required
+              className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-orange-500 focus:outline-none focus:ring-orange-500 sm:text-sm"
+              placeholder="Email address"
+            />
+            <label htmlFor="password" className="sr-only">
+              Password
+            </label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              required
+              className="relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-orange-500 focus:outline-none focus:ring-orange-500 sm:text-sm"
+              placeholder="Password"
+            />
+            <label htmlFor="confirm-password" className="sr-only">
+              Confirm password
+            </label>
+            <input
+              id="confirm-password"
+              name="confirm-password"
+              type="password"
+              autoComplete="current-password"
+              required
+              className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-orange-500 focus:outline-none focus:ring-orange-500 sm:text-sm"
+              placeholder="Confirm password"
+            />
           </div>
-          <div>
-            <button
-              type="submit"
-              className="group relative flex w-full justify-center rounded-md border border-transparent bg-orange-400 py-2 px-4 text-sm font-medium text-white hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
-            >
-              {isLoading ? 'Loading...' : 'Register'}
-            </button>
-          </div>
+          <button
+            type="submit"
+            className="group relative flex w-full justify-center rounded-md border border-transparent bg-orange-400 py-2 px-4 text-sm font-medium text-white hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
+          >
+            {isLoading ? 'Loading...' : 'Register'}
+          </button>
         </form>
         {isSuccess && <p>Registered successfully</p>}
         {isError && <p>Invalid email or password</p>}
--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -17,66 +17,56 @@ export default function SignInPage() {
     <div className="h-screen flex justify-center items-center">
       <div className="w-full max-w-md">
         <div>
-          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-800">
             Sign in to your account
           </h2>
-          <p className="mt-2 text-center text-sm text-gray-600">
-            Or{' '}
-            <Link
-              href={config.routes.register}
-              className="font-medium text-orange-600 hover:text-orange-500"
-            >
-              register a new account
-            </Link>
-          </p>
+          <Link
+            className="block mt-2 text-center text-sm font-medium text-orange-400 hover:text-orange-500"
+            href={config.routes.register}
+          >
+            Or register a new account
+          </Link>
         </div>
         <form className="mt-8 space-y-6" onSubmit={handleOnSubmit}>
           <div className="-space-y-px rounded-md shadow-sm">
-            <div>
-              <label htmlFor="email-address" className="sr-only">
-                Email address
-              </label>
-              <input
-                id="email-address"
-                name="email"
-                type="email"
-                autoComplete="email"
-                required
-                className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-orange-500 focus:outline-none focus:ring-orange-500 sm:text-sm"
-                placeholder="Email address"
-              />
-            </div>
-            <div>
-              <label htmlFor="password" className="sr-only">
-                Password
-              </label>
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                required
-                className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-orange-500 focus:outline-none focus:ring-orange-500 sm:text-sm"
-                placeholder="Password"
-              />
-            </div>
+            <label htmlFor="email-address" className="sr-only">
+              Email address
+            </label>
+            <input
+              id="email-address"
+              name="email"
+              type="email"
+              autoComplete="email"
+              required
+              className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-orange-500 focus:outline-none focus:ring-orange-500 sm:text-sm"
+              placeholder="Email address"
+            />
+
+            <label htmlFor="password" className="sr-only">
+              Password
+            </label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              required
+              className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-orange-500 focus:outline-none focus:ring-orange-500 sm:text-sm"
+              placeholder="Password"
+            />
           </div>
-          <div className="text-sm">
-            <Link
-              href="/forgot-password"
-              className="font-medium text-orange-600 hover:text-orange-500"
-            >
-              Forgot your password?
-            </Link>
-          </div>
-          <div>
-            <button
-              type="submit"
-              className="group relative flex w-full justify-center rounded-md border border-transparent bg-orange-400 py-2 px-4 text-sm font-medium text-white hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
-            >
-              {isLoading ? 'Loading...' : 'Sign In'}
-            </button>
-          </div>
+          <Link
+            href="/forgot-password"
+            className="block mt-2 text-sm font-medium text-orange-400 hover:text-orange-500"
+          >
+            Forgot your password?
+          </Link>
+          <button
+            type="submit"
+            className="group relative flex w-full justify-center rounded-md border border-transparent bg-orange-400 py-2 px-4 text-sm font-medium text-white hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
+          >
+            {isLoading ? 'Loading...' : 'Sign In'}
+          </button>
         </form>
         {isError && <p>Invalid email or password</p>}
       </div>
